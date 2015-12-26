7c4338bcd814578f0a171508588457ae3d47f30c
Jason Blevins <jrblevin@sdf.org>
2015-12-26 00:48:34 -0500

    init.el: send-region-to-{omnifocus,fantastical}

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -242,6 +242,8 @@
 (global-set-key (kbd "C-c p") 'magit-push)
 (global-set-key (kbd "C-c t") 'time-stamp)
 (global-set-key (kbd "C-c T") 'titlecase-dwim)
+(global-set-key (kbd "C-c o") 'send-region-to-omnifocus)
+(global-set-key (kbd "C-c f") 'send-region-to-fantastical)
 
 ;;; Ispell:
 
@@ -1110,3 +1112,71 @@ most recent kill ring contents and leaves the cursor at %|."
   "! ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE\n"
   "! POSSIBILITY OF SUCH DAMAGE.\n"
   "\n")
+
+;;; Inter-App Communication:
+
+;; AppleScript support
+(defun applescript-quote-string (argument)
+  "Quote a string for passing as a string to AppleScript."
+  (if (or (not argument) (string-equal argument ""))
+      "\"\""
+    ;; Quote using double quotes, but escape any existing quotes or
+    ;; backslashes in the argument with backslashes.
+    (let ((result "")
+          (start 0)
+          end)
+      (save-match-data
+        (if (or (null (string-match "[^\"\\]" argument))
+                (< (match-end 0) (length argument)))
+            (while (string-match "[\"\\]" argument start)
+              (setq end (match-beginning 0)
+                    result (concat result (substring argument start end)
+                                   "\\" (substring argument end (1+ end)))
+                    start (1+ end))))
+        (concat "\"" result (substring argument start) "\"")))))
+
+;; Send region to OmniFocus
+(defun send-region-to-omnifocus (beg end)
+  "Send the selected region to OmniFocus.
+Use the first line of the region as the task name and the second
+and subsequent lines as the task note."
+  (interactive "r")
+  (let* ((region (buffer-substring-no-properties beg end))
+         (match (string-match "^\\(.*\\)$" region))
+         (name (substring region (match-beginning 1) (match-end 1)))
+         (note (if (< (match-end 0) (length region))
+                   (concat (substring region (+ (match-end 0) 1) nil) "\n\n")
+                 "")))
+    (do-applescript
+     (format "set theDate to current date
+              set taskName to %s
+              set taskNote to %s
+              set taskNote to (taskNote) & \"Added from Emacs on \" & (theDate as string)
+              tell front document of application \"OmniFocus\"
+                make new inbox task with properties {name:(taskName), note:(taskNote)}
+              end tell"
+             (applescript-quote-string name)
+             (applescript-quote-string note)))))
+
+;; Send region to Fantastical
+(defun send-region-to-fantastical (beg end)
+  "Send the selected region to Fantastical.
+Parse the first line to create the event and use the second
+and subsequent lines as the event note."
+  (interactive "r")
+  (let* ((region (buffer-substring-no-properties beg end))
+         (match (string-match "^\\(.*\\)$" region))
+         (event (substring region (match-beginning 1) (match-end 1)))
+         (notes (if (< (match-end 0) (length region))
+                   (concat (substring region (+ (match-end 0) 1) nil) "\n\n")
+                 "")))
+    (do-applescript
+     (format "set theDate to current date
+              set eventText to %s
+              set eventNotes to %s
+              set eventNotes to (eventNotes) & \"Added from Emacs on \" & (theDate as string)
+              tell application \"Fantastical\"
+                parse sentence (eventText) notes (eventNotes)
+              end tell"
+             (applescript-quote-string event)
+             (applescript-quote-string notes)))))
