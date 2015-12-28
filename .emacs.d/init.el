48b583d2b1f06f545a4e8af701a83656310dc02c
Jason Blevins <jrblevin@sdf.org>
2015-12-28 17:59:52 -0500

    init.el: Custom variables and faces

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1212,3 +1212,21 @@ and subsequent lines as the event note."
 ;;; Local configuration
 
 (load-file "~/.emacs.d/init-local.el")
+
+;;; Custom variables and faces:
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold :family "variable-pitch"))))
+ '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.8))))
+ '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.4))))
+ '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.2)))))
