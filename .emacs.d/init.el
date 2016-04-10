9f272a42281f2c4adc194a98fc2ca056d98698af
Jason Blevins <jrblevin@sdf.org>
2016-04-10 10:20:30 -0400

    Emacs: configure hippie-expand

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -257,6 +257,21 @@
 (global-set-key (kbd "C-c o") 'send-region-to-omnifocus)
 (global-set-key (kbd "C-c f") 'send-region-to-fantastical)
 
+;;; Hippie expand:
+
+(global-set-key "\M-/" 'hippie-expand)
+(setq hippie-expand-try-functions-list
+      '(try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-all-abbrevs
+        try-expand-list
+        try-expand-line
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
+
 ;;; Ispell:
 
 (setq ispell-program-name "aspell")
