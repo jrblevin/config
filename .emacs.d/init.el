9efa13d71de6f3a21740e65c20ae20686d7941f8
Jason Blevins <jrblevin@sdf.org>
2016-04-10 23:18:51 -0400

    Emacs: move customized variables to custom.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1362,20 +1362,8 @@ and subsequent lines as the event note."
 
 (load-file "~/.emacs.d/init-local.el")
 
-;;; Custom variables and faces:
+;;; Custom configuration
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold :family "variable-pitch"))))
- '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.8))))
- '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.4))))
- '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.2)))))
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
