e7cd26e7eaf855c27e3ef5053fb0dc52fcef5175
Jason Blevins <jrblevin@sdf.org>
2016-04-21 14:29:04 -0400

    Emacs: stop timing startup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,8 +12,6 @@
 ;; ~/.emacs.d/site-lisp          manually installed packages
 ;; ~/.emacs.d/themes             custom themes
 
-(defconst emacs-start-time (current-time))
-
 
 ;;; Basic Configuration:
 
@@ -1303,20 +1301,5 @@ and subsequent lines as the event note."
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
-
-;;; Post initialization
-
-(when window-system
-  (let ((elapsed (float-time (time-subtract (current-time)
-                                            emacs-start-time))))
-    (message "Loading %s...done (%.3fs)" load-file-name elapsed))
-
-  (add-hook 'after-init-hook
-            `(lambda ()
-               (let ((elapsed (float-time (time-subtract (current-time)
-                                                         emacs-start-time))))
-                 (message "Loading %s...done (%.3fs) [after-init]"
-                          ,load-file-name elapsed)))
-            t))
 
 ;;; init.el ends here
